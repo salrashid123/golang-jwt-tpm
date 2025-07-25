5f90d27fb35b849a15ca66d2f7d227cc4a4ec86d
sal rashid <salrashid123@gmail.com>
2025-07-25 07:32:26 -0700

    add ekparent sample
    Signed-off-by: sal rashid <salrashid123@gmail.com>

---
+++ example/go_keyfile_ekparent/main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"context"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"os"
+	"slices"
+	"time"
+
+	keyfile "github.com/foxboron/go-tpm-keyfiles"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/google/go-tpm-tools/simulator"
+	"github.com/google/go-tpm/tpm2"
+	"github.com/google/go-tpm/tpm2/transport"
+	"github.com/google/go-tpm/tpmutil"
+	tpmjwt "github.com/salrashid123/golang-jwt-tpm"
+)
+
+/*
+Load a key using https://github.com/Foxboron/go-tpm-keyfiles
+*/
+var (
+	tpmPath = flag.String("tpm-path", "127.0.0.1:2341", "Path to the TPM device (character device or a Unix socket).")
+	in      = flag.String("in", "/tmp/tpmkey.pem", "privateKey File")
+	keyPass = flag.String("keyPass", "bar", "passphrase for the key")
+)
+
+var TPMDEVICES = []string{"/dev/tpm0", "/dev/tpmrm0"}
+
+func OpenTPM(path string) (io.ReadWriteCloser, error) {
+	if slices.Contains(TPMDEVICES, path) {
+		return tpmutil.OpenTPM(path)
+	} else if path == "simulator" {
+		return simulator.GetWithFixedSeedInsecure(1073741825)
+	} else {
+		return net.Dial("tcp", path)
+	}
+}
+
+func main() {
+	run()
+}
+func run() {
+
+	flag.Parse()
+	ctx := context.Background()
+
+	log.Printf("======= Init  ========")
+
+	rwc, err := OpenTPM(*tpmPath)
+	if err != nil {
+		log.Fatalf("can't open TPM %q: %v", *tpmPath, err)
+	}
+	defer func() {
+		if err := rwc.Close(); err != nil {
+			log.Fatalf("can't close TPM %q: %v", *tpmPath, err)
+		}
+	}()
+
+	rwr := transport.FromReadWriter(rwc)
+
+	log.Printf("======= reading key from file ========")
+	c, err := os.ReadFile(*in)
+	if err != nil {
+		log.Fatalf("error reading private keyfile: %v", err)
+	}
+	key, err := keyfile.Decode(c)
+	if err != nil {
+		log.Fatalf("failed decoding key: %v", err)
+	}
+
+	primaryKey, err := tpm2.CreatePrimary{
+		PrimaryHandle: tpm2.TPMRHEndorsement,
+		InPublic:      tpm2.New2B(tpm2.RSAEKTemplate),
+	}.Execute(rwr)
+	if err != nil {
+		log.Fatalf("can't create primary: %v", err)
+	}
+
+	defer func() {
+		flushContextCmd := tpm2.FlushContext{
+			FlushHandle: primaryKey.ObjectHandle,
+		}
+		_, _ = flushContextCmd.Execute(rwr)
+	}()
+
+	// create a new session to load
+	load_session, load_session_cleanup, err := tpm2.PolicySession(rwr, tpm2.TPMAlgSHA256, 16)
+	if err != nil {
+		log.Fatalf("can't create policy session: %v", err)
+	}
+	defer load_session_cleanup()
+
+	_, err = tpm2.PolicySecret{
+		AuthHandle: tpm2.AuthHandle{
+			Handle: tpm2.TPMRHEndorsement,
+			Name:   tpm2.HandleName(tpm2.TPMRHEndorsement),
+			Auth:   tpm2.PasswordAuth([]byte("")),
+		},
+		PolicySession: load_session.Handle(),
+		NonceTPM:      load_session.NonceTPM(),
+	}.Execute(rwr)
+	if err != nil {
+		log.Fatalf("can't creating policysecret: %v", err)
+	}
+
+	regenRSAKey, err := tpm2.Load{
+		ParentHandle: tpm2.AuthHandle{
+			Handle: primaryKey.ObjectHandle,
+			Name:   tpm2.TPM2BName(primaryKey.Name),
+			Auth:   load_session,
+		},
+		InPublic:  key.Pubkey,
+		InPrivate: key.Privkey,
+	}.Execute(rwr)
+	if err != nil {
+		log.Fatalf("can't load rsa key: %v", err)
+	}
+
+	defer func() {
+		flushContextCmd := tpm2.FlushContext{
+			FlushHandle: regenRSAKey.ObjectHandle,
+		}
+		_, _ = flushContextCmd.Execute(rwr)
+	}()
+
+	log.Printf("primaryKey Name %s\n", base64.StdEncoding.EncodeToString(primaryKey.Name.Buffer))
+
+	se, err := tpmjwt.NewPolicyAuthValueAndDuplicateSelectSession(rwr, []byte(*keyPass), primaryKey.Name)
+	if err != nil {
+		log.Fatalf("can'create session: %v", err)
+	}
+
+	claims := &jwt.RegisteredClaims{
+		ExpiresAt: &jwt.NumericDate{time.Now().Add(time.Minute * 1)},
+		Issuer:    "test",
+	}
+
+	tpmjwt.SigningMethodTPMRS256.Override()
+	token := jwt.NewWithClaims(tpmjwt.SigningMethodTPMRS256, claims)
+
+	config := &tpmjwt.TPMConfig{
+		TPMDevice:   rwc,
+		Handle:      regenRSAKey.ObjectHandle,
+		AuthSession: se,
+	}
+
+	keyctx, err := tpmjwt.NewTPMContext(ctx, config)
+	if err != nil {
+		log.Fatalf("Unable to initialize tpmJWT: %v", err)
+	}
+
+	// optionally set a keyID
+	//token.Header["kid"] = config.GetKeyID()
+
+	tokenString, err := token.SignedString(keyctx)
+	if err != nil {
+		log.Fatalf("Error signing %v", err)
+	}
+	fmt.Printf("TOKEN: %s\n", tokenString)
+
+	// verify with TPM based publicKey
+	keyFunc, err := tpmjwt.TPMVerfiyKeyfunc(ctx, config)
+	if err != nil {
+		log.Fatalf("could not get keyFunc: %v", err)
+	}
+
+	vtoken, err := jwt.Parse(tokenString, keyFunc)
+	if err != nil {
+		log.Fatalf("Error verifying token %v", err)
+	}
+	if vtoken.Valid {
+		log.Println("     verified with TPM PublicKey")
+	}
+
+	// verify with provided RSAPublic key
+	pubKeyr := config.GetPublicKey()
+
+	v, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return pubKeyr, nil
+	})
+	if err != nil {
+		log.Fatalf("Error parsing token %v", err)
+	}
+	if v.Valid {
+		log.Println("     verified with exported PubicKey")
+	}
+}
